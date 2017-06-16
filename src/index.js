6075e67a84e7ff17b970d3392a8d9f8c1d2b2664
Sung <sungmkim80@gmail.com>
2017-06-15 20:26:58 -0400

    search works!

--- src/index.js
+++ src/index.js
@@ -16,9 +16,13 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({ key: API_KEY, term: 'algorithm' }, (videos) => {
-      this.setState({ 
-        videos: videos, 
+    this.videoSearch('algorithms');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
+      this.setState({
+        videos: videos,
         selectedVideo: videos[0]
       });
     });
@@ -27,10 +31,10 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
-        <VideoList 
-          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
           videos={this.state.videos} />
       </div>
     );
